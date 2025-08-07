fbf0bc37b97f73cd4feaea65b277e7f428bc89d1
ItsDuska <olli.sotavalta@gmail.com>
2025-08-07 12:18:16 +0300

    kala gaming

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+set -g theme_nerd_fonts yes
+set -g theme_display_git_master_branch yes
+set -g theme_color_scheme dark
+
+set -gx PATH $HOME/.local/bin $PATH
+
+if type -q starship
+    starship init fish | source
 end
--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -g theme_nerd_fonts yes
-set -g theme_display_git_master_branch yes
-set -g theme_color_scheme dark
-
-set -gx PATH $HOME/.local/bin $PATH
-
-if type -q starship
-    starship init fish | source
-end
